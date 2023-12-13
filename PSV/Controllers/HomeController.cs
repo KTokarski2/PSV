9d79e1e6f070975489d443a88f2c2d605a0bced3
KTokarski2 <krzysztof.tokarski2@icloud.com>
2023-12-13 08:34:08 +0100

    initial commit

---
+++ PSV/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace PSV.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    
+}
