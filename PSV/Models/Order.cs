19dc7e4bdc7f0789fde1d4445ae34a70deaa820e
KTokarski2 <krzysztof.tokarski2@icloud.com>
2023-12-14 16:28:18 +0100

    create data model

---
+++ PSV/Models/Order.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PSV.Models;
+
+public class Order
+{
+    [Key]
+    public int Id { get; set; }
+    public int IdClient { get; set; }
+    public virtual Client Client { get; set; }
+    public string QrCode { get; set; }
+    public string Format { get; set; }
+    public string Comments { get; set; }
+    public string Photos { get; set; }
+    public virtual Cut Cut { get; set; }
+    public virtual Milling Milling { get; set; }
+    public virtual Wrapping Wrapping { get; set; }
+}
